573fb6c5ccd989551d50549e90eb32c6c6e10181
Zhengtang Yang <zyang888@uw.edu>
2019-12-09 00:45:32 -0800

    google verify fix height

--- equitygeneral.js
+++ equitygeneral.js
@@ -3,7 +3,7 @@ function initMap() {
   var everett = {lat: 47.963, lng: -122.200556};
   // The map, centered at Uluru
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: everett});
+      document.getElementById('map'), {zoom: 13, center: everett});
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({position: everett, map: map});
 }
